4fee95cbe17382d9aea925a8ec5cab08c3807144
JJ Merelo <jjmerelo@gmail.com>
2016-12-27 14:56:04 +0100

    Changing package configuration

--- cask-bootstrap.el
+++ cask-bootstrap.el
@@ -57,8 +57,9 @@
         (condition-case nil
             (mapc 'require cask-bootstrap-packages)
           (error
-           (add-to-list 'package-archives (cons "gnu" "https://elpa.gnu.org/packages/"))
-           (add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
+	   (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+           (add-to-list 'package-archives (cons "gnu" "http://elpa.gnu.org/packages/"))
+           (add-to-list 'package-archives (cons "melpa" "http://melpa.org/packages/"))
            (package-refresh-contents)
            (mapc
             (lambda (package)
